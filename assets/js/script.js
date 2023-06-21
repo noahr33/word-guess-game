60a50bf9bf56a39b7199d78c58b889966e8a89b0
Noah <noahkoleske@icloud.com>
2023-06-20 21:02:17 -0500

    added variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
 // vars
 
+
+
+
 // select dom elements
-  // start button
-  // reset button
-  // count down timer
-  // word-blanks
-  // wins
-  // losses
+var startButton = document.querySelector('.start-button')
+var resetButton = document.querySelector('.reset-button')
+var timerEl = document.querySelector('.timer')
+var wordBlanksEl = document.querySelector('.word-blanks')
+var winsEl = document.querySelector('.win')
+var lossesEl = document.querySelector('.lose')
+
+console.log(startButton, resetButton, timerEl, wordBlanksEl, winsEl, lossesEl);
 
   // list of words
   // userGuesses = [] list of letters guessed by user
